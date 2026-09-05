0e014ce8ab0057e25528bfd933db9e21609b88b6
divyaguled202 <divyaguleddivya@gmail.com>
2026-09-05 23:29:39 +0530

    Updated project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,7 +23,7 @@ const Projects = () => {
     description: 'Frontend clone of Amazon with product layout, cart UI, and responsive design built using HTML, CSS, and JavaScript.',
     tech: ['HTML', 'CSS', 'JavaScript'],
     github: 'https://github.com/divyaguled202',
-    demo: 'https://amazon-clone-eta-plum.vercel.app/',
+    demo: 'https://amazon-clone-fnbxpxsup-divya-8b20.vercel.app/',
     image: amazonImg
   },
   {
